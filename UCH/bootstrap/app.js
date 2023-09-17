e6ca7eaf2e461a12192aa48bea14d0a8fae28836
Mikhail Mryasov <damaghack@gmail.com>
2023-09-17 17:09:13 +0700

    U

--- UCH/bootstrap/app.js
+++ UCH/bootstrap/app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
+const userRouter = require("./routers/userRouter.js");
+const homeRouter = require("./routers/homeRouter.js");
 
-app.use(express.static(__dirname + "/www"));
+app.set("View engine", "hbs");
+app.use(express.urlencoded({ extended: false }));
 
-app.listen(3000);
+app.use("/users", userRouter);
+app.use("/", homeRouter);
+
+app.use(function (req, res, next) {
+    res.status(404).send("Not found");
+});
+
+app.listen(3000, () => console.log("Server started and waiting connection..."));
